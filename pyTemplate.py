df6fdd036a990f2013ae797234fc8a833dfa8b26
JSuelfl <Justin.Suelflow@bmo.com>
2016-04-14 10:27:06 -0500

    Create pyTemplate.py

---
+++ pyTemplate.py
@@ -0,0 +1,52 @@
+##*********************HEADER*********************##
+##Developer             : 
+##Date                  : 
+##Program Name          : 
+##Description           : 
+##Python Version        : 
+##Prereqs Knowledge     : 
+##Prereqs Hardware      : 
+##Prereqs Software      : 
+##Python Libraries      : 
+##Needed Python Files   :
+##Log File              :
+##CSV File              :
+##Static variables      :
+##Run Python Command    :
+##-----------------------------------------------------------------------------
+##  Version  |  mm/dd/yyyy  |       User       |                Changes
+##
+##-----------------------------------------------------------------------------
+##*********************END HEADER*********************##
+
+##*********************IMPORT*********************##
+##  Import needed python libraries
+##  Libraries must be installed using 'pip install'
+
+##*********************END IMPORT*********************##
+
+##*********************FUNCTIONS*********************##
+
+##  Function	: 
+##  Description	: 
+##  Parameters	: 
+##  Returns	: 
+
+##*********************MAIN FUNCTION*********************##
+##  Function	: 
+##  Description	: 
+##  Parameters	: 
+##  Returns	: 
+def main():
+	
+##*********************END MAIN FUNCTION*********************##
+
+##*********************END FUNCTIONS*********************##
+
+##*********************PROGRAM*********************##
+##  If statement makes this program standalone
+##  Do not need this if statement if another program will be calling above functions
+if __name__ == "__main__":
+        main()
+
+##*********************END PROGRAM*********************##
